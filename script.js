254463032c02a07126f00f619642e27539ea96f2
igor-vieira-car <igor@carvalho.sptech.school>
2023-06-18 12:27:09 -0300

    atualização da lógica n completa

--- script.js
+++ script.js
@@ -1,297 +1,59 @@
+var vetorDica = ["1 - Tem dois núcleos de processamento, permitindo que o processador execute várias tarefas simultaneamente",
+    "2 - São pequenas áreas de armazenamento dentro da CPU usadas para guardar dados temporários, resultados e endereços de memória.",
+    "3 - Tem quatro núcleos, permitindo que o processador execute várias tarefas simultaneamente.",
+    "4 - É a unidade central de processamento, responsável por executar as instruções e coordenar as operações de um computador",
+    "5 - São processadores fabricados pela Intel Corporation, podendo ser quad-core ou hexa-core, com suporte para hyper-threading.",
+    "6 - Memória de apenas leitura, contendo dados ou instruções permanentes.",
+    "7 - Memória regravável eletronicamente.",
+    "8 - São unidades de execução dentro de um processo, permitindo a execução simultânea de tarefas para melhor desempenho e capacidade de resposta.",
+    "9 - É um sinal de controle usado para selecionar um dispositivo específico em um barramento compartilhado.",
+    "10 - É uma memória rápida usada para armazenar dados frequentemente acessados pela CPU, reduzindo a latência de acesso à memória principal e melhorando o desempenho do sistema.",
+    "11 - São componentes do barramento do sistema. O Address Bus transporta informações sobre o endereço de memória desejado, e o Data Bus transporta os dados entre a CPU e a memória ou dispositivos de E/S.",
+    "12 - é um componente do barramento do sistema responsável por transmitir os dados entre a CPU, a memória e osdispositivos de entrada/saída. Ele permite a comunicação bidirecional e é essencial para transferir informaçõesdentro do sistema.",
+    "13 - É a unidade lógica e aritmética dentro da CPU, responsável por realizar operações matemáticas e lógicas.",
+    "14 - Memória de armazenamento permanente, como discos rígidos e SSDs.",
+    "15 - São processadores fabricados pela Intel Corporation, geralmente sendo quad-core.",
+    "16 - Direct Memory Access é uma técnica que permite que dispositivos acessem diretamente a memória principal sem passar pela CPU, melhorando a eficiência do sistema.",
+]
+var palavras = ["quadcore", "registradores", "dualcore", "cpu", "i7", "rom", "eprom", "threads", "cs", "cache", "addressbus", "databus", "ula", "memoriademassa", "i5", "dma"]
 
-    function verificar() {
-        var registradores = letra46.value + letra47.value + letra48.value + letra49.value + letra50.value + letra51.value + letra52.value + letra53.value + letra54.value + letra55.value + letra56.value + letra57.value + letra58.value;
+var dicaVetor = 0;
 
-        var i = 1;
-
-        if (registradores.toLowerCase() == "registradores") {
-            while (i <= 13) {
-
-                document.getElementById(`letra${i + 45}`).style.border = "#00ff00 2px solid"
-                i++;
-            }
-        } else {
-            while (i <= 13) {
-                document.getElementById(`letra${i + 45}`).style.border = "#ff0000 2px solid"
-                i++;
-            }
+function dicas(num) {
+    var textoConcatenado = "";
+    for (var i = 1; i <= 87; i++) {
+        if (document.getElementById(`letra${i}`).disabled == false) {
+            document.getElementById(`letra${i}`).disabled = true;
         }
+        document.getElementById(`letra${i}`).style.border = "#cccccc solid 1px";
 
-
-
-        var ram = letra46.value + letra61.value + letra67.value;
-        if (ram == "ram") {
-            document.getElementById(`letra46`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra61`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra67`).style.border = "#00ff00 2px solid";
-        } else {
-            document.getElementById(`letra46`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra61`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra67`).style.border = "#ff0000 2px solid";
-
-        }
-
-
-
-        var rom = letra65.value + letra66.value + letra67.value;
-        if (rom == "rom") {
-            document.getElementById(`letra65`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra66`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra67`).style.border = "#00ff00 2px solid";
-        } else {
-            document.getElementById(`letra65`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra66`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra67`).style.border = "#ff0000 2px solid";
-        }
-
-
-        var eprom = letra45.value + letra60.value + letra65.value + letra70.value + letra75.value;
-        if (eprom == "eprom") {
-            document.getElementById(`letra45`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra60`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra65`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra70`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra75`).style.border = "#00ff00 2px solid";
-        } else {
-            document.getElementById(`letra45`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra60`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra65`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra70`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra75`).style.border = "#ff0000 2px solid";
-        }
-
-
-
-
-        var i7 = letra49.value + letra62.value;
-
-        if (i7 == "i7") {
-            document.getElementById(`letra49`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra62`).style.border = "#00ff00 2px solid";
-        } else {
-            document.getElementById(`letra49`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra62`).style.border = "#ff0000 2px solid";
-        }
-
-
-
-        var quadcore = letra1.value + letra2.value + letra3.value + letra12.value + letra20.value + letra23.value + letra41.value + letra47.value;
-
-        if (quadcore == "quadcore") {
-            document.getElementById(`letra1`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra2`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra3`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra12`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra20`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra23`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra41`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra47`).style.border = "#00ff00 2px solid";
-        } else {
-            document.getElementById(`letra1`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra2`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra3`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra12`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra20`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra23`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra41`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra47`).style.border = "#ff0000 2px solid";
-        }
-
-        var dualcore = letra24.value + letra42.value + letra53.value + letra63.value + letra71.value + letra76.value + letra77.value + letra86.value
-
-        if (dualcore == "dualcore") {
-            document.getElementById(`letra24`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra42`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra53`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra63`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra71`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra76`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra77`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra86`).style.border = "#00ff00 2px solid";
-        } else {
-            document.getElementById(`letra24`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra42`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra53`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra63`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra71`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra76`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra77`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra86`).style.border = "#ff0000 2px solid";
-        }
-
-
-        var cpu = letra71.value + letra72.value + letra73.value
-        if (cpu == "cpu") {
-
-            document.getElementById(`letra71`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra72`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra73`).style.border = "#00ff00 2px solid";
-        } else {
-            document.getElementById(`letra71`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra72`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra73`).style.border = "#ff0000 2px solid";
-        }
-
-
-        var threads = letra83.value + letra84.value + letra85.value + letra86.value + letra87.value + letra88.value + letra89.value
-        if (threads == "threads") {
-            i = 1;
-            while (i <= 6) {
-                document.getElementById(`letra${i + 82}`).style.border = "#00ff00 2px solid";
-                i++;
-            }
-        } else {
-            i = 1;
-            while (i <= 7) {
-                document.getElementById(`letra${i + 82}`).style.border = "#ff0000 2px solid";
-                i++;
-            }
-        }
-
-
-        var cs = letra78.value + letra89.value;
-
-        if (cs == "cs") {
-
-            document.getElementById(`letra78`).style.border = "#00ff00 2px solid";
-            document.getElementById(`letra89`).style.border = "#00ff00 2px solid";
-        } else {
-            document.getElementById(`letra78`).style.border = "#ff0000 2px solid";
-            document.getElementById(`letra89`).style.border = "#ff0000 2px solid";
-        }
-
-        var cache = letra78.value + letra79.value + letra80.value + letra81.value + letra82.value;
-
-        if (cache == "cache") {
-            i = 1;
-            while (i <= 5) {
-
-                document.getElementById(`letra${i + 77}`).style.border = "#00ff00 2px solid";
-                i++
-            }
-        } else {
-
-            i = 1;
-            while (i <= 5) {
-
-                document.getElementById(`letra${i + 77}`).style.border = "#ff0000 2px solid";
-                i++
-            }
-        }
-        letra1
-
-        var databus = letra4.value + letra5.value + letra6.value + letra7.value + letra9.value + letra10.value + letra11.value;
-
-        if (databus == 'databus') {
-            i = 1;
-            while (i <= 8) {
-                document.getElementById(`letra${i + 3}`).style.border = "#00ff00 2px solid";
-                i++;
-            }
-        } else {
-            i = 1;
-            while (i <= 8) {
-                document.getElementById(`letra${i + 3}`).style.border = "#ff0000 1px solid";
-                i++;
-            }
-        }
-
-        var ula = letra10.value + letra14.value + letra18.value
-
-        if (ula == 'ula') {
-            document.getElementById(`letra${10}`).style.border = "#00ff00 1px solid";
-            document.getElementById(`letra${14}`).style.border = "#00ff00 1px solid";
-            document.getElementById(`letra${18}`).style.border = "#00ff00 1px solid";
-        } else {
-            document.getElementById(`letra${10}`).style.border = "#ff0000 1px solid";
-            document.getElementById(`letra${14}`).style.border = "#ff0000 1px solid";
-            document.getElementById(`letra${18}`).style.border = "#ff0000 1px solid";
-        }
-
-
-
-        var memoria = letra25.value + letra26.value + letra27.value + letra28.value + letra29.value + letra30.value + letra31.value + letra33.value+ letra34.value+ letra36.value+ letra37.value+ letra38.value + letra39.value + letra40.value;
-
-        if (memoria == 'memoriademassa') {
-            i = 1;
-            while (i <= 16) {
-                document.getElementById(`letra${i + 24}`).style.border = "#00ff00 2px solid";
-                i++;
-            }
-        } else {
-            i = 1;
-            while (i <= 16) {
-                document.getElementById(`letra${i + 24}`).style.border = "#ff0000 2px solid";
-                i++;
-            }
-        }
-
-        var addressbus = letra5.value + letra13.value + letra17.value + letra21.value + letra26.value + letra43.value + letra58.value + letra64.value + letra69.value + letra74.value
-        
-        if(addressbus == "addressbus"){
-            document.getElementById(`letra5`).style.border = "#00ff00 1px solid";
-            document.getElementById(`letra13`).style.border = "#00ff00 1px solid";
-            document.getElementById(`letra17`).style.border = "#00ff00 1px solid";
-            document.getElementById(`letra21`).style.border = "#00ff00 1px solid";
-            document.getElementById(`letra26`).style.border = "#00ff00 1px solid";
-            document.getElementById(`letra43`).style.border = "#00ff00 1px solid";
-            document.getElementById(`letra58`).style.border = "#00ff00 1px solid";
-            document.getElementById(`letra64`).style.border = "#00ff00 1px solid";
-            document.getElementById(`letra69`).style.border = "#00ff00 1px solid";
-            document.getElementById(`letra74`).style.border = "#00ff00 1px solid";
-        }else{
-            document.getElementById(`letra5`).style.border = "#ff0000 1px solid";
-            document.getElementById(`letra13`).style.border = "#ff0000 1px solid";
-            document.getElementById(`letra17`).style.border = "#ff0000 1px solid";
-            document.getElementById(`letra21`).style.border = "#ff0000 1px solid";
-            document.getElementById(`letra26`).style.border = "#ff0000 1px solid";
-            document.getElementById(`letra43`).style.border = "#ff0000 1px solid";
-            document.getElementById(`letra58`).style.border = "#ff0000 1px solid";
-            document.getElementById(`letra64`).style.border = "#ff0000 1px solid";
-            document.getElementById(`letra69`).style.border = "#ff0000 1px solid";
-            document.getElementById(`letra74`).style.border = "#ff0000 1px solid";
-
-        }
-
-        // var flash = letra15.value + letra19.value + letra22.value + letra40.value + letra44.value + letra59.value
-
-        // if(flash == 'flash'){
-
-        //     document.getElementById(`letra15`).style.border = "#00ff00 1px solid";
-        //     document.getElementById(`letra19`).style.border = "#00ff00 1px solid";
-        //     document.getElementById(`letra22`).style.border = "#00ff00 1px solid";
-        //     document.getElementById(`letra40`).style.border = "#00ff00 1px solid";
-        //     document.getElementById(`letra44`).style.border = "#00ff00 1px solid";
-        //     document.getElementById(`letra59`).style.border = "#00ff00 1px solid";
-
-        // }else{
-        //     document.getElementById(`letra15`).style.border = "#ff0000 1px solid";
-        //     document.getElementById(`letra19`).style.border = "#ff0000 1px solid";
-        //     document.getElementById(`letra22`).style.border = "#ff0000 1px solid";
-        //     document.getElementById(`letra40`).style.border = "#ff0000 1px solid";
-        //     document.getElementById(`letra44`).style.border = "#ff0000 1px solid";
-        //     document.getElementById(`letra59`).style.border = "#ff0000 1px solid";
-        // }
-
-        var i5 = letra30.value + letra44.value;
-
-        if(i5 == "i5"){
-            document.getElementById(`letra30`).style.border = "#00ff00 1px solid";
-            document.getElementById(`letra44`).style.border = "#00ff00 1px solid";
-        }else{
-            document.getElementById(`letra30`).style.border = "#ff0000 1px solid";
-            document.getElementById(`letra44`).style.border = "#ff0000 1px solid";
-        }
-
-        var dma = letra19.value + letra22.value + letra40.value;
-
-        if(dma == "dma"){
-            document.getElementById(`letra19`).style.border = "#00ff00 1px solid";
-            document.getElementById(`letra22`).style.border = "#00ff00 1px solid";
-            document.getElementById(`letra40`).style.border = "#00ff00 1px solid";
-        }else{
-            document.getElementById(`letra19`).style.border = "#ff0000 1px solid";
-            document.getElementById(`letra22`).style.border = "#ff0000 1px solid";
-            document.getElementById(`letra40`).style.border = "#ff0000 1px solid";
-        }
     }
+
+
+    if (dicaVetor == 0 && num == -1) {
+        dicaVetor = vetorDica.length - 1;
+    } else if (dicaVetor == vetorDica.length - 1 && num == 1) {
+        dicaVetor = 0; s
+    } else {
+        dicaVetor = dicaVetor + num;
+    }
+    span_dica.innerHTML = `${vetorDica[dicaVetor]}`
+
+    var inputs = document.getElementsByClassName(`${palavras[dicaVetor]}`);
+
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].style.border = "#000000 solid 1px"; // Altere "red" para a cor desejada
+        document.getElementsByClassName(`${palavras[dicaVetor]}`).disabled = false;
+        textoConcatenado += inputs[i].value;
+        console.log(inputs[i].value);
+    }
+    alert(textoConcatenado);
+
+}
+
+
+
+
+
+
+
